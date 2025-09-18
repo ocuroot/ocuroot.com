97b14efc9f53eb16587a1dd75ecf8fc8c0d761d9
Tom Elliott <tom.w.elliott@gmail.com>
2025-09-18 15:43:45 -0400

    Clean up embedding on components
    Now all files are embedded so any new .css or .js files are picked up automatically.

--- site/embed.go
+++ site/embed.go
@@ -1,44 +1,75 @@
 package site
 
 import (
+	"embed"
 	_ "embed"
+	"io/fs"
+	"log"
+	"sort"
+	"strings"
 
 	"github.com/ocuroot/ui/css"
 	"github.com/ocuroot/ui/js"
 )
 
-//go:embed style.css
-var styleCSS []byte
-
-//go:embed mermaid.css
-var mermaidCSS []byte
-
-//go:embed syntax-highlighting.css
-var syntaxHighlightingCSS []byte
-
-//go:embed gitcard.css
-var gitcardCSS []byte
-
-//go:embed gitcard.js
-var gitcardJS []byte
-
-//go:embed components/code/code.css
-var codeCSS []byte
-
-//go:embed components/code/code.js
-var codeJS []byte
-
-//go:embed newsletter.css
-var newsletterCSS []byte
+//go:embed **
+var allFiles embed.FS
 
 func init() {
-	css.Default().Add(styleCSS)
-	css.Default().Add(mermaidCSS)
-	css.Default().Add(syntaxHighlightingCSS)
-	css.Default().Add(gitcardCSS)
-	css.Default().Add(codeCSS)
-	css.Default().Add(newsletterCSS)
+	// Walk the embedded filesystem to collect all CSS and JS files
+	cssFiles := make(map[string]string) // name -> path
+	jsFiles := make(map[string]string)  // name -> path
 
-	js.Default().Add(gitcardJS)
-	js.Default().Add(codeJS)
+	err := fs.WalkDir(allFiles, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !d.IsDir() {
+			name := d.Name()
+			if strings.HasSuffix(name, ".css") {
+				cssFiles[name] = path
+			} else if strings.HasSuffix(name, ".js") {
+				jsFiles[name] = path
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to walk embedded filesystem: %v", err)
+	}
+
+	// Get sorted file names for stable output
+	var cssNames []string
+	for name := range cssFiles {
+		cssNames = append(cssNames, name)
+	}
+	sort.Strings(cssNames)
+
+	var jsNames []string
+	for name := range jsFiles {
+		jsNames = append(jsNames, name)
+	}
+	sort.Strings(jsNames)
+
+	// Add each CSS file to the default CSS registry
+	for _, name := range cssNames {
+		path := cssFiles[name]
+		content, err := allFiles.ReadFile(path)
+		if err != nil {
+			log.Fatalf("Failed to read CSS file %s: %v", path, err)
+		}
+		css.Default().Add(content)
+	}
+
+	// Add each JS file to the default JS registry
+	for _, name := range jsNames {
+		path := jsFiles[name]
+		content, err := allFiles.ReadFile(path)
+		if err != nil {
+			log.Fatalf("Failed to read JS file %s: %v", path, err)
+		}
+		js.Default().Add(content)
+	}
 }
